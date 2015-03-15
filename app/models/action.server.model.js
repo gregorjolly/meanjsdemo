5f832d7798a298413fe84afee88a8b2447849928
Gregor <gregorjolly@me.com>
2015-03-15 09:33:01 +0000

    Inital commit of base project

---
+++ app/models/action.server.model.js
@@ -0,0 +1,29 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+	Schema = mongoose.Schema;
+
+/**
+ * Action Schema
+ */
+var ActionSchema = new Schema({
+	name: {
+		type: String,
+		default: '',
+		required: 'Please fill Action name',
+		trim: true
+	},
+	created: {
+		type: Date,
+		default: Date.now
+	},
+	user: {
+		type: Schema.ObjectId,
+		ref: 'User'
+	}
+});
+
+mongoose.model('Action', ActionSchema);
